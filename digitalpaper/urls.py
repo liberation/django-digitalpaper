21414f3ad1c839dc974534f104ce01be6554f4e3
Mathieu Pillard <m.pillard@liberation.fr>
2010-09-27 16:18:50 +0200

    Basic redirection to latest publication (using pk) on /reader/

--- digitalpaper/urls.py
+++ digitalpaper/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('digitalpaper.views',
     url(r'^reader/(?P<publication_id>[\d]+)/$', 'reader', name='digitalpaper_reader'),
+    url(r'^reader/$', 'reader_latest', name='digitalpaper_reader_latest'),    
 )
--- digitalpaper/views.py
+++ digitalpaper/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
 from digitalpaper.models import publication_model
+
+def reader_latest(request):
+    pub = publication_model.objects.latest('pk')
+    return redirect('digitalpaper_reader', publication_id=pub.pk)
 
 def reader(request, publication_id):
     p = get_object_or_404(publication_model, pk=int(publication_id))
