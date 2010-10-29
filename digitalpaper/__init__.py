f784e9140e2cc58ddec67fb7d879d810786423e7
Mathieu Pillard <m.pillard@liberation.fr>
2010-10-29 19:40:10 +0200

    General cleanup of the various ways of accessing publications. (Backwards-incompatible change!)

--- digitalpaper/__init__.py
+++ digitalpaper/__init__.py
@@ -7,16 +7,16 @@ def get_publication_date_field():
 
 def get_model_for_publication():
     return loading.get_model(settings.READER_PUBLICATION_APP_NAME, settings.READER_PUBLICATION_MODEL_NAME)
-    
+   
 def get_manager_for_publication(inst):
     name = getattr(settings, 'READER_PUBLICATION_MANAGER_NAME', 'objects')
     manager = getattr(get_model_for_publication(), name)
     return manager
-
-def get_manager_method_for_publication(inst):
+   
+def get_manager_method_for_publication_by_date(inst):
     manager = get_manager_for_publication(inst)
-    method = getattr(manager, settings.READER_PUBLICATION_MANAGER_METHOD_NAME)
-    return method()
+    method = getattr(manager, settings.READER_PUBLICATION_MANAGER_METHOD_BYDATE_NAME)
+    return method
     
 publication_model = get_model_for_publication()
 
--- digitalpaper/views.py
+++ digitalpaper/views.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from datetime import datetime
 
 from digitalpaper import publication_model, \
-                         get_manager_method_for_publication, \
-                         get_publication_date_field
+                         get_manager_for_publication, \
+                         get_manager_method_for_publication_by_date, \
+                         get_publication_date_field \
 
 def reader_latest(request):
-    pub = get_manager_method_for_publication(publication_model).latest()
+    pub = get_manager_for_publication(publication_model).latest()
     return redirect('digitalpaper_reader', publication_id=pub.pk)
     
 def reader_date(request, date):
-    manager = get_manager_method_for_publication(publication_model)
+    method = get_manager_method_for_publication_by_date(publication_model)
     filters = {}
-    filters[get_publication_date_field()] = date
-    pub = get_object_or_404(manager, **filters)    
-    return redirect('digitalpaper_reader', publication_id=pub.pk)    
+    filters[get_publication_date_field()] = datetime.strptime(date, '%Y-%m-%d')
+    pub = method(**filters)
+    if not pub:
+        raise Http404
+    
+    return redirect('digitalpaper_reader', publication_id=pub.pk)
 
 def reader(request, publication_id):
-    manager = get_manager_method_for_publication(publication_model)
+    manager = get_manager_for_publication(publication_model)
     pub = get_object_or_404(manager, pk=int(publication_id))
     context = {
         'publication': pub,
+        'latest_publication' : get_manager_for_publication(publication_model).latest()
     }
     return render_to_response('digitalpaper/reader.html', context, context_instance=RequestContext(request))
