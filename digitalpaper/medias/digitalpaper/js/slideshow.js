1076623a305f24a8cef3f522fc808e1406a7719b
Olivier Mobili <o.mobili@libe.fr>
2010-11-03 15:27:24 +0100

    Improve the page slider, by recalculating the position dynamically and preventing scrolling while the animation is taking place.

--- digitalpaper/medias/digitalpaper/js/slideshow.js
+++ digitalpaper/medias/digitalpaper/js/slideshow.js
@@ -1,25 +1,39 @@
-jQuery(document).ready(function () {
-    iSlideSize = 296;
-    sSlideSpeed = 'slow';
-    iPosition = 0;
+jQuery(document).ready(function() {
+    var iSlideSize = 296;
+    var sSlideSpeed = 'slow';
+    var isMoving = false;
 
     jQuery('#sliderPrev').bind('click', function(e) {
-        iPosition += iSlideSize;
-        minPosition = 0
-        if (iPosition > minPosition) {
-            iPosition = minPosition;
-        }
-        jQuery('#pagesList').animate({left: iPosition}, sSlideSpeed);
-        return false;
-    });
-    jQuery('#sliderNext').bind('click', function(e) {
-        iPosition -= iSlideSize;
-        maxPosition = jQuery('#pagesList').outerWidth() - jQuery('#innerPagesSlider').outerWidth();
-        if (maxPosition > 0) {
-            if (iPosition < -maxPosition) {
-                iPosition = -maxPosition;
+        if (!isMoving) {
+            isMoving = true;
+            var iPosition = (jQuery('#pagesList').css('left').replace('px', '') * 1) + iSlideSize;
+            var minPosition = 0;
+
+            if (iPosition > minPosition) { 
+                iPosition = minPosition;
             }
-            jQuery('#pagesList').animate({left: iPosition}, sSlideSpeed);
+
+            jQuery('#pagesList').animate({ left: iPosition }, sSlideSpeed, function() {
+                isMoving = false;
+            });
+        }
+        return false ;
+    }) ;
+
+    jQuery('#sliderNext').bind('click', function(e) {
+        if (!isMoving) {
+            var maxPosition = jQuery('#pagesList').outerWidth() - jQuery('#innerPagesSlider').outerWidth();
+            if (maxPosition > 0) {
+                isMoving = true ;
+                var iPosition = jQuery('#pagesList').css('left').replace('px', '') - iSlideSize;
+                if (iPosition < -maxPosition) { 
+                    iPosition = -maxPosition;
+                }
+
+                jQuery('#pagesList').animate({ left: iPosition }, sSlideSpeed, function() {
+                    isMoving = false ;
+                });
+            }
         }
         return false;
     });
