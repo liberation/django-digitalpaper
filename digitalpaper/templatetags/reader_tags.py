7f31a905989fb14a2060f35383f36fdb5a6fd30b
Mathieu Pillard <m.pillard@liberation.fr>
2012-06-19 20:31:49 +0200

    Add dummy template tags used to get the preview URLs for paperpages and publications
    (Don't want to declare named URLs just yet - might in the future though, and that
     would render this bit useless.)

--- digitalpaper/templatetags/reader_tags.py
+++ digitalpaper/templatetags/reader_tags.py
@@ -1,6 +1,8 @@
  # -*- coding: utf-8 -*-
 
 from django import template
+from django.utils.html import conditional_escape
+
 from digitalpaper.utils import get_publication_date_field
 
 register = template.Library()
@@ -9,3 +11,14 @@ register = template.Library()
 def get_publication_date(o):
     return getattr(o, get_publication_date_field())
 
+@register.simple_tag
+def paperpage_preview_absolute_url(paperpage, size='x148'):
+    if not paperpage:
+        return ""
+    return conditional_escape(paperpage.get_preview_absolute_url(size=size))
+
+@register.simple_tag
+def publication_preview_absolute_url(publication, size='x148'):
+    if not publication:
+        return ""
+    return conditional_escape(publication.get_preview_absolute_url(size=size))
